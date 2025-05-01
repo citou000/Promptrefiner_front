7bc3624d503faee24d2f7f8c3d591f17d274b313
citou000 <citoujean@gmail.com>
2025-05-01 18:25:31 +0000

    update the UI info

--- src/components/contextInput.vue
+++ src/components/contextInput.vue
@@ -62,7 +62,7 @@ ${context.value.trim()}
 
     loading.value = true
     try {
-      const res = await fetch('https://promptrefiner-back.onrender.com/generate', {
+      const res = await fetch('http://localhost:3000/generate', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ prompt: finalPrompt.value }),
